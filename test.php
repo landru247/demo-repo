60d336eae46f217f2b5df30a56dcf134023ba0a5
Landru <landru247@gmail.com>
2021-11-02 15:53:14 -0700

    5

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 $derp = 'some string of text';
 $merp = 'what is going on with this';
 
-if ($derp = $merp) {
+if ($derp !== $merp) {
   echo 'hey you look like a snerp!';
 } else {
   echo 'do something else';
